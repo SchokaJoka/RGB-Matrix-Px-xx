e1e9c8751f8e482d2ad2a8705313ee2555cf7db4
Apisana <64089856+api-sana@users.noreply.github.com>
2026-06-16 17:55:36 +0200

    show platform

--- example/Rasberry-Pi/examples-api-use/train-demo.cc
+++ example/Rasberry-Pi/examples-api-use/train-demo.cc
@@ -168,7 +168,7 @@ const int x_base = matrix_->width() - panel_width;
 const int x_dest = x_base + 2;
 const int x_time = x_base + 35;
 // const int x_delay = x_time + 20;
-const int x_plat = x_base + 55;
+const int x_plat = x_time + 20;
 
     if (!ok || trains.empty()) {
       DrawLineText(x_offset, 0, Color(255, 0, 0), "Train board error");

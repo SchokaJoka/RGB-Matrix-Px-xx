87ab8a6e0df47361f24c24b3bffdc3d6374b3392
Apisana <64089856+api-sana@users.noreply.github.com>
2026-06-16 17:20:53 +0200

    fix overlay

--- example/Rasberry-Pi/examples-api-use/train-demo.cc
+++ example/Rasberry-Pi/examples-api-use/train-demo.cc
@@ -166,9 +166,9 @@ private:
 const int x_base = matrix_->width() - panel_width;
 
 const int x_dest = x_base + 2;
-const int x_time = x_base + 28;
-const int x_delay = x_time + 20;
-const int x_plat = x_base + 50;
+const int x_time = x_base + 35;
+const int x_delay = x_time + 25;
+const int x_plat = x_base + 55;
 
     if (!ok || trains.empty()) {
       DrawLineText(x_offset, 0, Color(255, 0, 0), "Train board error");
